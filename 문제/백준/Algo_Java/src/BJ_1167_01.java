54f7f6c9f3c7e20b7519c93517362dd2857eddc4
feelincoding <feelincoding@gmail.com>
2022-06-21 10:00:05 +0900

    add: dfs,bfs

---
+++ 문제/백준/Algo_Java/src/BJ_1167_01.java
@@ -0,0 +1,67 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BJ_1167_01 {
+    static int N;
+    static int[][] treeArr;
+    static int[][] isVisited;
+    static int result = 0;
+    
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        treeArr = new int[N + 1][N + 1];
+        isVisited = new int[N + 1][N + 1];
+
+        for (int i = 1; i < N + 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int node = Integer.parseInt(st.nextToken());
+            while (true) {
+                int nodeNum = Integer.parseInt(st.nextToken());
+                if (nodeNum == -1)
+                    break;
+                int cost = Integer.parseInt(st.nextToken());
+                treeArr[node][nodeNum] = cost;
+            }
+        }
+
+        for (int i = 1; i < N + 1; i++) {
+            dfs(i, 0);
+        }
+
+        System.out.println(result);
+    }
+
+    public static void dfs(int row, int tmpResult) {
+        for (int i = row; i < N + 1; i++) {
+            if (isVisited[row][i] == 1)
+                continue;
+            isVisited[row][i] = 1;
+            if (treeArr[row][i] != 0) {
+                dfs(i, tmpResult + treeArr[row][i]);
+            } else {
+                result = Math.max(result, tmpResult);
+            }
+            // if (treeArr[row][i] == 0) {
+            // result = Math.max(result, tmpResult + treeArr[i][0]);
+            // } else {
+            // dfs(i, tmpResult + treeArr[row][i]);
+            // }
+        }
+    }
+
+    public class Node {
+        int node;
+        int cost;
+
+        public Node(int node, int cost){
+            this.node = node;
+            this.cost = cost;
+        }
+    }
+}
---
+++ 문제/백준/Algo_Java/src/BJ_1260_01.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ_1260_01 {
+    static int N, M, V;
+    static int[][] arr;
+    static boolean[] isVisited;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        V = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1][N + 1];
+        isVisited = new boolean[N + 1];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            arr[v1][v2] = 1;
+            arr[v2][v1] = 1;
+        }
+        dfs(V);
+        System.out.println(sb.toString());
+        sb = new StringBuilder();
+        isVisited = new boolean[N + 1];
+        bfs(V);
+        System.out.println(sb.toString());
+    }
+
+    private static void dfs(int v) {
+        if (!isVisited[v]) {
+            isVisited[v] = true;
+            sb.append(Integer.toString(v) + " ");
+            for (int i = 1; i < N + 1; i++) {
+                if (arr[v][i] == 1 && !isVisited[i]) {
+                    dfs(i);
+                }
+            }
+        } else
+            return;
+    }
+
+    private static void bfs(int v) {
+        Queue<Integer> q = new LinkedList<>();
+        isVisited[v] = true;
+        q.add(v);
+        while (true) {
+            if (q.isEmpty())
+                break;
+            int v1 = q.poll();
+            sb.append(Integer.toString(v1) + " ");
+            for (int i = 1; i < N + 1; i++) {
+                if (arr[v1][i] == 1 && !isVisited[i]) {
+                    isVisited[i] = true;
+                    q.add(i);
+                }
+            }
+        }
+    }
+}
