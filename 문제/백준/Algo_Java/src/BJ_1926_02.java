614639ca0e6d42ae7035c369a21008306c6043a8
feelincoding <feelincoding@gmail.com>
2022-06-30 09:12:14 +0900

    feat: ??

--- 문제/백준/Algo_Java/src/BJ_1926_02.java
+++ 문제/백준/Algo_Java/src/BJ_1926_02.java
@@ -8,64 +8,73 @@ public class BJ_1926_02 {
     static int[] dx;
     static int[] dy;
     static Queue<int[]> qu;
+    static int count = 0;
+    static int area = 0;
+    static int max = 0;
 
-    public static void main(String args[]) throws IOException{
+    public static void main(String args[]) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(bf.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        
+
         arr = new int[n][m];
         visit = new boolean[n][m];
         qu = new LinkedList<>();
-        
-        dx = new int[]{1, 0, -1, 0};
-        dy = new int[]{0, 1, 0, -1};
-        
+
+        dy = new int[] { 0, 1, 0, -1 };
+        dx = new int[] { 1, 0, -1, 0 };
+
         // 배열값 세팅
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(bf.readLine());
-            for(int j=0; j<m; j++){
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        
+
         // 시작!
-        int count = 0;
-        int area = 0;
-        int max = 0;
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
+        count = 0;
+        area = 0;
+        max = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 // 0이거나 방문한적이 있으면 생략.
-                if(arr[i][j] == 0 || visit[i][j]){
+                if (arr[i][j] == 0 || visit[i][j]) {
                     continue;
                 }
-                count++;    // 1이고, 방문을 하지 않았으므로 시작점이 되니까 +1
-                qu.offer(new int[]{i, j});   // 큐에 좌표를 넣어준다.
-                visit[i][j] = true;     // 방문처리!
-                area = 0;    // 0이면 그림이 끊겼으므로 area = 0!
-                while(!qu.isEmpty()){
-                    int[] p = qu.poll();
-                    area++; // 넓이 +1
-                    for(int k = 0; k < 4; k++){
-                        int n_x = p[0] + dx[k];
-                        int n_y = p[1] + dy[k];
-                        if(n_x < 0 || n_x >= n || n_y < 0 || n_y >= m){
-                            continue;
-                        }
-                        if(arr[n_x][n_y] == 1 && !visit[n_x][n_y]){
-                            qu.offer(new int[]{n_x, n_y});
-                            visit[n_x][n_y] = true;
-                        }
-                    }
-                }
-                if(area > max){
-                    max = area;
-                }
+                bfs(i, j);
             }
-            
+
         }
         System.out.println(count);
         System.out.println(max);
+    }
+
+    public static void bfs(int i, int j) {
+        count++; // 1이고, 방문을 하지 않았으므로 시작점이 되니까 +1
+        qu.offer(new int[] { i, j }); // 큐에 좌표를 넣어준다.
+        visit[i][j] = true; // 방문처리!
+        area = 0; // 0이면 그림이 끊겼으므로 area = 0!
+        while (!qu.isEmpty()) {
+            int[] p = qu.poll();
+            area++; // 넓이 +1
+            for (int k = 0; k < 4; k++) {
+                int n_y = p[0] + dy[k];
+                int n_x = p[1] + dx[k];
+                if (n_y < 0 || n_y >= n || n_x < 0 || n_x >= m)
+                    continue;
+
+                if (arr[n_y][n_x] == 0 || visit[n_y][n_x])
+                    continue;
+
+                qu.offer(new int[] { n_y, n_x });
+                visit[n_y][n_x] = true;
+
+            }
+        }
+        if (area > max) {
+            max = area;
+        }
     }
 }
---
+++ 문제/백준/Algo_Java/src/BJ_1926_03.java
@@ -0,0 +1,81 @@
+import java.io.*;
+import java.util.*;
+
+public class BJ_1926_03 {
+    // static int n, m, count, maxArea, area;
+    // static int[][] canvas;
+    // static boolean[][] isVisited;
+    // static int[] dy = { 0, 1, 0, -1 }; // 오른쪽부터 시계방향
+    // static int[] dx = { 1, 0, -1, 0 };
+    // static Queue<int[]> q = new LinkedList<>();
+
+    static int n, m;
+    static int[][] canvas;
+    static boolean[][] isVisited;
+    static int[] dx;
+    static int[] dy;
+    static Queue<int[]> q;
+    static int count = 0;
+    static int area = 0;
+    static int maxArea = 0;
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        canvas = new int[n][m];
+        isVisited = new boolean[n][m];
+        q = new LinkedList<>();
+
+        dy = new int[] { 0, 1, 0, -1 };
+        dx = new int[] { 1, 0, -1, 0 };
+
+        // 배열값 세팅
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                canvas[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        // 시작!
+        count = 0;
+        area = 0;
+        maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (canvas[i][j] == 0 || isVisited[i][j]) {
+                    continue;
+                } else {
+                    count++; // 1이고, 방문을 하지 않았으므로 시작점이 되니까 +1
+                    q.offer(new int[] { i, j }); // 큐에 좌표를 넣어준다.
+                    isVisited[i][j] = true; // 방문처리!
+                    area = 0; // 0이면 그림이 끊겼으므로 area = 0!
+                    while (!q.isEmpty()) {
+                        int[] p = q.poll();
+                        area++; // 넓이 +1
+                        for (int k = 0; k < 4; k++) {
+                            int n_y = p[0] + dy[k];
+                            int n_x = p[1] + dx[k];
+                            if (n_y < 0 || n_y >= n || n_x < 0 || n_x >= m)
+                                continue;
+
+                            if (canvas[n_y][n_x] == 0 || isVisited[n_y][n_x])
+                                continue;
+
+                            q.offer(new int[] { n_y, n_x });
+                            isVisited[n_y][n_x] = true;
+
+                        }
+                    }
+                    if (area > maxArea) {
+                        maxArea = area;
+                    }
+                }
+            }
+        }
+        System.out.println(count);
+        System.err.println(maxArea);
+    }
+}
