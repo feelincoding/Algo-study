b47733e4badff52b24679bbce888dc280188c0bb
feelincoding <feelincoding@gmail.com>
2022-11-29 17:59:57 +0900

    add-5397_01

---
+++ 문제/2022/Algo_Java/src/bj/ch04_LinkedList/BJ_5397_01.java
@@ -0,0 +1,16 @@
+package bj.ch04_LinkedList;
+
+import java.util.*;
+import java.io.*;
+
+public class BJ_5397_01 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < n; i++) {
+            LinkedList<Character> list  = new LinkedList<>();
+            st = new StringTokenizer(br.readLine());
+        }
+    }
+}
