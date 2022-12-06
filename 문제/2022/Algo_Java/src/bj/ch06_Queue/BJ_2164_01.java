755fd681ab40ec9af7c935738632ef85a39d6182
feelincoding <feelincoding@gmail.com>
2022-12-06 18:01:09 +0900

    add-2164:풀이중

---
+++ 문제/2022/Algo_Java/src/bj/ch06_Queue/BJ_2164_01.java
@@ -0,0 +1,12 @@
+package bj.ch06_Queue;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_2164_01 {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+    }
+}
