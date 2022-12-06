0bca7a3747589ef2377a1fd55d7c7a69d883f645
feelincoding <feelincoding@gmail.com>
2022-12-06 16:40:15 +0900

    add-18258: 구현형식으로도 풀어보기

--- 문제/2022/Algo_Java/src/bj/ch06_Queue/BJ_18258_01.java
+++ 문제/2022/Algo_Java/src/bj/ch06_Queue/BJ_18258_01.java
@@ -1,5 +1,39 @@
 package bj.ch06_Queue;
 
+import java.io.*;
+import java.util.*;
+
 public class BJ_18258_01 {
-    
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        LinkedList<String> q = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            String command = st.nextToken();
+            switch (command) {
+                case "push":
+                    q.offer(st.nextToken());
+                    break;
+                case "pop":
+                    sb.append(q.isEmpty() ? "-1\n" : q.pop() + "\n");
+                    break;
+                case "size":
+                    sb.append(q.size() + "\n");
+                    break;
+                case "empty":
+                    sb.append(q.isEmpty() ? 1 + "\n" : 0 + "\n");
+                    break;
+                case "front":
+                    sb.append(q.isEmpty() ? "-1\n" : q.peek() + "\n");
+                    break;
+                default:
+                    sb.append(q.isEmpty() ? "-1\n" : q.peekLast() + "\n");
+                    break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
 }
---
+++ 문제/2022/Algo_Java/src/bj/ch06_Queue/BJ_18258_02.java
@@ -0,0 +1,12 @@
+package bj.ch06_Queue;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ_18258_02 { // 구현형식으로 풀어보기
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+    }
+}
