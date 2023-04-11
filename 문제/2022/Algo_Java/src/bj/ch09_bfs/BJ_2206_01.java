c912a2517da902e6a70d9e797208bb3337801364
feelincoding <feelincoding@gmail.com>
2023-04-11 22:42:54 +0900

    add: 2206 ~ing

--- 문제/2022/Algo_Java/src/bj/ch09_bfs/BJ_2206_01.java
+++ 문제/2022/Algo_Java/src/bj/ch09_bfs/BJ_2206_01.java
@@ -1,5 +1,15 @@
 package bj.ch09_bfs;
 
+import java.util.*;
+import java.io.*;
+
+/*
+ * 한 칸 부술 수 있음
+ * 안 부순 경우
+ * 하나 부순 경우
+ * 
+ */
 public class BJ_2206_01 {
-    
+    public static int[] dx = { 0, 0, -1, 1 };
+    public static int[] dy = { -1, 1, 0, 0 };
 }
