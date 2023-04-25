544c07f5bbda1c8d66d86975be93856222186f87
feelincoding <feelincoding@gmail.com>
2023-04-25 16:41:45 +0900

    docs: 이해 완료....

--- 문제/2022/Algo_Java/src/bj/ch11_recursion/BJ_1629_02_BarkingDog.java
+++ 문제/2022/Algo_Java/src/bj/ch11_recursion/BJ_1629_02_BarkingDog.java
@@ -16,17 +16,15 @@ public class BJ_1629_02_BarkingDog {
         System.out.println(pow(A, B, C));
     }
 
-    private static long pow(int a, int b, int c) {
+    private static long pow(int a, int b, int c) { // a^b mod c = (a^(b/2) mod c * a^(b/2) mod c) mod c
         if (b == 1) {
             return a % c;
         }
-        long val = a * pow(a, b / 2, c);
-        val = val * val % c;
-        if (b % 2 == 0) {
+        long val = pow(a, b / 2, c);
+        val = val * val % c; // a^b mod c = (a^(b/2) mod c * a^(b/2) mod c) mod c
+        if (b % 2 == 0) { // 짝수면 그대로
             return val;
         }
-        return val * a % c;
+        return val * a % c; // 홀수면 한번 더 a, mod c 해줘야지
     }
-
-    
 }
